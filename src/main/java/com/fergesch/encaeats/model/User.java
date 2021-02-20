3be6df5ad9d72e1ed75f6fbfb95c54c70fad636e
Erica Kresch <ekresch15@gmail.com>
2021-02-20 12:27:36 -0600

    adding missing annotation

--- src/main/java/com/fergesch/encaeats/model/User.java
+++ src/main/java/com/fergesch/encaeats/model/User.java
@@ -2,9 +2,11 @@ package com.fergesch.encaeats.model;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
 public class User {
     String id;
     String name;
